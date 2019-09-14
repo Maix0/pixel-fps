97c8ee62b39413e5c20937bd15b12372148b7696
maix0 <maix522@gmail.com>
2019-09-14 12:31:29 +0000

    remove multithreading (+75% fps)

--- src/main.rs
+++ src/main.rs
@@ -140,137 +140,136 @@ fn game_logic(game: &mut engine::Engine) -> Result<(), String> {
         }
 
         for x in 0..=(game.size.0) {
-            rayon::scope(|s| {
-                s.spawn(|_| {
-                    //ray_angle =
-                    //    (player.angle - player.fov / 2.0_f64) + ((x / game.size.0) as f64) * player.fov;
-                    let ray_angle = (player.angle - player.fov / 2.0_f64)
-                        + (x as f64 / game.size.0 as f64) * player.fov;
-                    let mut wall_distance = 0_f64;
-                    let stepsize = 0.1_f64;
-                    let mut hit_wall = false;
-                    let mut boundry = false;
-                    let eye_x = ray_angle.sin();
-                    let eye_y = ray_angle.cos();
-                    let mut boundry_vec: Vec<(f64, f64)> = Vec::new();
-                    boundry_vec.clear();
-                    while !hit_wall && wall_distance < player.depth {
-                        wall_distance += stepsize;
-                        let test_x = (player.x + eye_x * wall_distance).floor() as i64;
-                        let test_y = (player.y + eye_y * wall_distance).floor() as i64;
+            //rayon::scope(|s| {
+            //    s.spawn(|_| {
+            //ray_angle =
+            //    (player.angle - player.fov / 2.0_f64) + ((x / game.size.0) as f64) * player.fov;
+            let ray_angle = (player.angle - player.fov / 2.0_f64)
+                + (x as f64 / game.size.0 as f64) * player.fov;
+            let mut wall_distance = 0_f64;
+            let stepsize = 0.1_f64;
+            let mut hit_wall = false;
+            let mut boundry = false;
+            let eye_x = ray_angle.sin();
+            let eye_y = ray_angle.cos();
+            let mut boundry_vec: Vec<(f64, f64)> = Vec::new();
+            boundry_vec.clear();
+            while !hit_wall && wall_distance < player.depth {
+                wall_distance += stepsize;
+                let test_x = (player.x + eye_x * wall_distance).floor() as i64;
+                let test_y = (player.y + eye_y * wall_distance).floor() as i64;
 
-                        if test_x < 0 || test_x >= map.w || test_y < 0 || test_y >= map.h {
-                            hit_wall = true;
-                            wall_distance = player.depth;
-                        } else {
-                            if map.get_2d(test_x, test_y) == Some('#') {
-                                hit_wall = true;
-                                boundry_vec.clear();
-                                // /*
-                                for tx in 0..2 {
-                                    for ty in 0..2 {
-                                        vy = (test_y as f64) + ty as f64 - player.y;
-                                        vx = (test_x as f64) + tx as f64 - player.x;
-                                        d = (vx * vx + vy * vy).sqrt();
-                                        dot = (eye_x * vx / d) + (eye_y * vy / d);
-                                        boundry_vec.push((d, dot));
-                                    }
-                                }
-                                boundry_vec.sort_by(|left: &(f64, f64), right: &(f64, f64)| {
-                                    left.0.partial_cmp(&right.0).unwrap()
-                                });
-
-                                let bound = 0.01_f64;
-                                if boundry_vec[0].1.acos() < bound {
-                                    //println!("{:?}", boundry_vec[0].1.acos());
-                                    boundry = true;
-                                }
-                                if boundry_vec[1].1.acos() < bound {
-                                    //println!("{:?}", boundry_vec[1].1.acos());
-                                    boundry = true;
-                                }
-                                if boundry_vec[2].1.acos() < bound {
-                                    //println!("{:?}", boundry_vec[2].1.acos());
-                                    boundry = true;
-                                }
-                                // */
+                if test_x < 0 || test_x >= map.w || test_y < 0 || test_y >= map.h {
+                    hit_wall = true;
+                    wall_distance = player.depth;
+                } else {
+                    if map.get_2d(test_x, test_y) == Some('#') {
+                        hit_wall = true;
+                        boundry_vec.clear();
+                        // /*
+                        for tx in 0..2 {
+                            for ty in 0..2 {
+                                vy = (test_y as f64) + ty as f64 - player.y;
+                                vx = (test_x as f64) + tx as f64 - player.x;
+                                d = (vx * vx + vy * vy).sqrt();
+                                dot = (eye_x * vx / d) + (eye_y * vy / d);
+                                boundry_vec.push((d, dot));
                             }
                         }
-                    }
-                    let ceiling = ((game.size.1 as f64 / 2.0) as f64
-                        - game.size.1 as f64 / wall_distance)
-                        as i64;
-                    let floor = (game.size.1 as i64 - ceiling) as i64;
+                        boundry_vec.sort_by(|left: &(f64, f64), right: &(f64, f64)| {
+                            left.0.partial_cmp(&right.0).unwrap()
+                        });
 
-                    let mut col = engine::Color::BLACK;
-                    if wall_distance <= player.depth / 4.0_f64 {
-                        col = engine::Color::WHITE;
-                    } else if wall_distance < player.depth / 3.0_f64 {
-                        //println!("grey");
-                        col = engine::Color::GREY;
-                    } else if wall_distance < player.depth / 2.0_f64 {
-                        //println!("dark");
-                        col = engine::Color::DARK_GREY;
-                    } else if wall_distance < player.depth {
-                        //println!("darker");
-                        col = engine::Color::VERY_DARK_GREY;
+                        let bound = 0.01_f64;
+                        if boundry_vec[0].1.acos() < bound {
+                            //println!("{:?}", boundry_vec[0].1.acos());
+                            boundry = true;
+                        }
+                        if boundry_vec[1].1.acos() < bound {
+                            //println!("{:?}", boundry_vec[1].1.acos());
+                            boundry = true;
+                        }
+                        if boundry_vec[2].1.acos() < bound {
+                            //println!("{:?}", boundry_vec[2].1.acos());
+                            boundry = true;
+                        }
+                        // */
+                    }
+                }
+            }
+            let ceiling =
+                ((game.size.1 as f64 / 2.0) as f64 - game.size.1 as f64 / wall_distance) as i64;
+            let floor = (game.size.1 as i64 - ceiling) as i64;
+
+            let mut col = engine::Color::BLACK;
+            if wall_distance <= player.depth / 4.0_f64 {
+                col = engine::Color::WHITE;
+            } else if wall_distance < player.depth / 3.0_f64 {
+                //println!("grey");
+                col = engine::Color::GREY;
+            } else if wall_distance < player.depth / 2.0_f64 {
+                //println!("dark");
+                col = engine::Color::DARK_GREY;
+            } else if wall_distance < player.depth {
+                //println!("darker");
+                col = engine::Color::VERY_DARK_GREY;
+            } else {
+                //println!("black");
+                col = engine::Color::BLACK;
+            }
+
+            if boundry {
+                //println!("boundry");
+                if wall_distance <= player.depth / 4.0_f64 {
+                    col = engine::Color::GREY;
+                } else if wall_distance < player.depth / 3.0_f64 {
+                    col = engine::Color::DARK_GREY;
+                } else if wall_distance < player.depth / 2.0_f64 {
+                    col = engine::Color::VERY_DARK_GREY;
+                } else if wall_distance < player.depth {
+                    col = engine::Color::new(32, 32, 32);
+                } else {
+                    col = engine::Color::BLACK;
+                }
+            }
+
+            for y in 0..=((game).size.1) {
+                if y as i64 <= ceiling {
+                    // CEILING
+                    game.screen
+                        .draw(x as i32, y as i32, engine::Color::BLACK)
+                        .expect("Error while drawing to screen");
+                } else if y as i64 > ceiling && y as i64 <= floor {
+                    // WALL
+                    game.screen
+                        .draw(x as i32, y as i32, col)
+                        .expect("Error while drawing to screen");
+                } else {
+                    // FLOOR
+                    b = 1.0_f64
+                        - ((y as f64) - (&game).size.1 as f64 / 2.0_f64)
+                            / (((&game).size.1 as f64 / 2.0_f64) as f64);
+
+                    if b < 0.25 {
+                        col = engine::Color::BLUE;
+                    } else if b < 0.5 {
+                        col = engine::Color::DARK_BLUE;
+                    } else if b < 0.75 {
+                        col = engine::Color::VERY_DARK_BLUE;
+                    } else if b < 0.9 {
+                        col = engine::Color::new(0, 0, 32);
                     } else {
-                        //println!("black");
                         col = engine::Color::BLACK;
                     }
 
-                    if boundry {
-                        //println!("boundry");
-                        if wall_distance <= player.depth / 4.0_f64 {
-                            col = engine::Color::GREY;
-                        } else if wall_distance < player.depth / 3.0_f64 {
-                            col = engine::Color::DARK_GREY;
-                        } else if wall_distance < player.depth / 2.0_f64 {
-                            col = engine::Color::VERY_DARK_GREY;
-                        } else if wall_distance < player.depth {
-                            col = engine::Color::new(32, 32, 32);
-                        } else {
-                            col = engine::Color::BLACK;
-                        }
-                    }
-
-                    for y in 0..=((game).size.1) {
-                        if y as i64 <= ceiling {
-                            // CEILING
-                            game.screen
-                                .draw(x as i32, y as i32, engine::Color::BLACK)
-                                .expect("Error while drawing to screen");
-                        } else if y as i64 > ceiling && y as i64 <= floor {
-                            // WALL
-                            game.screen
-                                .draw(x as i32, y as i32, col)
-                                .expect("Error while drawing to screen");
-                        } else {
-                            // FLOOR
-                            b = 1.0_f64
-                                - ((y as f64) - (&game).size.1 as f64 / 2.0_f64)
-                                    / (((&game).size.1 as f64 / 2.0_f64) as f64);
-
-                            if b < 0.25 {
-                                col = engine::Color::BLUE;
-                            } else if b < 0.5 {
-                                col = engine::Color::DARK_BLUE;
-                            } else if b < 0.75 {
-                                col = engine::Color::VERY_DARK_BLUE;
-                            } else if b < 0.9 {
-                                col = engine::Color::new(0, 0, 32);
-                            } else {
-                                col = engine::Color::BLACK;
-                            }
-
-                            //col = engine::Color::new(0, 0, (255_f64 - (255_f64 * b)) as u8);
-                            game.screen
-                                .draw(x as i32, y as i32, col)
-                                .expect("Error while drawing to screen");
-                        }
-                    }
-                });
-            });
+                    //col = engine::Color::new(0, 0, (255_f64 - (255_f64 * b)) as u8);
+                    game.screen
+                        .draw(x as i32, y as i32, col)
+                        .expect("Error while drawing to screen");
+                }
+            }
+            //    });
+            //});
             for ny in 0..=(map.h - 1) {
                 for nx in 0..=(map.w - 1) {
                     //println!("{:?}, {:?} : {:?}", nx, ny, map.get(ny * map.w + nx));
